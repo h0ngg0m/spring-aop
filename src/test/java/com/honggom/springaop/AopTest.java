b9fd3552e0a73dcbdd82faa1b80c96e1b82325b3
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-28 15:58:28 +0900

    :tada: init

---
+++ src/test/java/com/honggom/springaop/AopTest.java
@@ -0,0 +1,39 @@
+package com.honggom.springaop;
+
+import com.honggom.springaop.order.OrderRepository;
+import com.honggom.springaop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderSservice: {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository: {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("예외 발생");
+    }
+}
